2893fe23c204dec04dee4af1291ea57e23b7b1ec
abdo-mhmd <abdo202224@gmail.com>
2023-07-15 07:01:25 -0700

    python tasks done

---
+++ python-hello_world/3-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+Last_digit = abs(number) % 10
+if Last_digit > 5:
+    print("Last digit of" ,number, "is" ,Last_digit, "and is greater than 5", end="\n")
+elif Last_digit == 0:
+    print("Last digit of" ,number, "is" ,Last_digit, "and is 0", end="\n")
+else:
+    print("Last digit of" ,number, "is" ,Last_digit, "and is less than 6 and not 0", end="\n")
