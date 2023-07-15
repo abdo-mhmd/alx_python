28e463081624428f7459b0612391b820e2dd3838
abdo-mhmd <abdo202224@gmail.com>
2023-07-15 07:25:38 -0700

    update last digit task

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,9 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 Last_digit = abs(number) % 10
-if Last_digit > 5:
-    print("Last digit of" ,number, "is" ,Last_digit, "and is greater than 5", end="\n")
+print("Last digit of", number, "is", end=" ")
+if Last_digit > 5 and number > 0:
+    print(Last_digit, "and is greater than 5", end="\n")
 elif Last_digit == 0:
-    print("Last digit of" ,number, "is" ,Last_digit, "and is 0", end="\n")
+    print(Last_digit, "and is 0", end="\n")
 else:
-    print("Last digit of" ,number, "is" ,Last_digit, "and is less than 6 and not 0", end="\n")
+    if number > 0:
+        print(Last_digit, "and is less than 6 and not 0", end="\n")
+    else:
+        print(-Last_digit, "and is less than 6 and not 0", end="\n")
