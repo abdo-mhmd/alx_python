8c21b037cafad1cbeca50431f6c6785d8995e186
abdo-mhmd <abdo202224@gmail.com>
2023-07-22 11:53:32 -0700

    update division by zero

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    if b == 0:
-        return (None)
-    else:
-        return (a / b)
+    try:
+        if b != 0:
+            return (a / b)
+        else:
+            return (None)
+    except ZeroDivisionError as ex:
+        print(ex)
+    finally:
+        if b != 0:
+            print("Inside result: {}".format(a / b))
+        else:
+            print("Inside result: {}".format(None))
