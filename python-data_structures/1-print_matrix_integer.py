b27bb40181a7b3680954077371bc84b07f2fe758
abdo-mhmd <abdo202224@gmail.com>
2023-07-22 13:45:40 -0700

    update format

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     i = 0
     while i < len(matrix):
         for x in matrix[i]:
-            print("{} ".format(x), end="")
+            print("{:d} ".format(x), end="")
         print("")
         i += 1
