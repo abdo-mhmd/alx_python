855289651dd23599306285845e7f46fbc64c222e
abdo-mhmd <abdo202224@gmail.com>
2023-07-23 09:50:29 -0700

    best scores

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionart == {}:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return (None)
     else:
         hight_score = max(list(a_dictionary.values()))
