20e5fc90b7b943c76b5760dad0cafd03b0c317eb
abdo-mhmd <abdo202224@gmail.com>
2023-07-29 08:37:42 -0700

    complete square task

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.__size = self.is_int(size)
+
+    def is_int(self, size):
+        if isinstance(size, int):
+            return (self.is_negative(size))
+        else:
+            raise TypeError("size must be an integer")
+
+    def is_negative(self, size):
+        if size >= 0:
+            return (size)
+        else:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return (self.__size * self.__size)
---
+++ python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.__size = size   
+
+    def is_int(self, size):
+        if isinstance(size, int):
+            return (self.is_negative(size))
+        else:
+            raise TypeError("size must be an integer")
+
+    def is_negative(self, size):
+        if size >= 0:
+            return (size)
+        else:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return (self.__size ** 2)
+    @property
+    def size(self): 
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        self.__size = self.is_int(value)
+
+    def my_print(self):
+        if self.__size is 0:
+            print() 
+        else:
+            for row in range(self.__size):
+                for i in range(self.__size):
+                    print("#", end="")
+                print("\n")
