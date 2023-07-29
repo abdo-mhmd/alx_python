b3cdd15a22edf2506a2f4f5e0fdcfcb3a6bb168f
abdo-mhmd <abdo202224@gmail.com>
2023-07-29 15:30:12 -0700

    edit

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 """This is a sample module named my_module.
 
 This module contains a class and a function.
@@ -9,9 +8,14 @@ class BaseGeometry:
     """
     A base class representing basic geometry operations.
 
-    This class serves as a foundation for specific geometry-related classes.
-    It contains the `integer_validator` method to validate positive integers.
-
     Attributes:
         None
     """
+    def __init_subclass__(cls):
+        """
+        Override the __init_subclass__ method with an empty method.
+
+        This will effectively remove the
+        __init_subclass__ method from the class.
+        """
+        pass
