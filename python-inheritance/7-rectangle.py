2533ca95cedd70638343ae1d607fdf1f84b6048c
abdo-mhmd <74677767+abdo-mhmd@users.noreply.github.com>
2023-07-30 17:58:35 +0300

    Update 7-rectangle.py

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -48,7 +48,6 @@ class Rectangle(BaseGeometry):
         self.__height = height
         self.integer_validator("width", self.__width)
         self.integer_validator("height", self.__height)
-        self.__dict__
 
     def area(self):
         """
