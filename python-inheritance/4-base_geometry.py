f91fd4c4af5eb4da36817ea7ba295dc30fecefb5
abdo-mhmd <abdo202224@gmail.com>
2023-07-30 06:43:56 -0700

    update 4base remove init subclass

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -54,3 +54,26 @@ class BaseGeometry:
                     return self.__width * self.__height
         """
         raise Exception("area() is not implemented")
+
+    def __dir__(self):
+        """
+        Override the dir() method to exclude
+        '__init_subclass__' from the list of attributes.
+
+        This method is called when using the dir()
+        function on an instance of BaseGeometry.
+        It returns a custom list of attributes without
+        including '__init_subclass__'.
+
+        Returns:
+            List[str]: A list of strings containing
+            the names of the instance attributes.
+
+        Usage:
+            # When calling dir() on an instance of BaseGeometry,
+            '__init_subclass__' will not be included.
+            bg = BaseGeometry()
+            dir(bg)
+        """
+        attributes = super().__dir__()
+        return [attr for attr in attributes if attr != '__init_subclass__']
