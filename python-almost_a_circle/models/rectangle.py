ab21f448b417dae0faf1385ff7888e22e3f95f24
abdo-mhmd <abdo202224@gmail.com>
2023-08-05 19:24:11 +0300

    update value

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,65 +1,71 @@
-#!/usr/bin/python3
 from models.base import Base
 '''rectangle'''
 
 
 class Rectangle(Base):
-
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
         return self.__width
 
     @width.setter
-    def width(self, width):
-        if type(width) is not int:
-            raise TypeError("width must be an")
-        elif width <= 0:
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
             raise ValueError("width must be > 0")
-        else:
-            self.__width = width
+        self.__width = value
 
     @property
     def height(self):
         return self.__height
 
     @height.setter
-    def height(self, height):
-        if type(height) is not int:
-            raise TypeError("height must be an")
-        elif height <= 0:
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
             raise ValueError("height must be > 0")
-        else:
-            self.__height = height
+        self.__height = value
 
     @property
     def x(self):
         return self.__x
 
     @x.setter
-    def x(self, x):
-        if type(x) is not int:
-            raise TypeError("x must be an")
-        elif x < 0:
+    def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
             raise ValueError("x must be >= 0")
-        else:
-            self.__x = x
+        self.__x = value
 
     @property
     def y(self):
         return self.__y
 
     @y.setter
-    def y(self, y):
-        if type(y) is not int:
-            raise TypeError("y must be an")
-        elif y < 0:
+    def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
             raise ValueError("y must be >= 0")
-        else:
-            self.__y = y
+        self.__y = value
+
+    # def area(self):
+    #     return self.width * self.height
+
+    # def display(self):
+    #     for _ in range(self.y):
+    #         print()
+    #     for _ in range(self.height):
+    #         print(" " * self.x + "#" * self.width)
+    
+    # def __str__(self):
+    #     return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
