1f4e063b7acb5970986ba864070103d34d52d71e
abdo-mhmd <abdo202224@gmail.com>
2023-08-10 22:28:13 +0300

    update error code

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -13,9 +13,10 @@ as the base for other classes.
 
 
 url = sys.argv[1]
+
 response = requests.get(url)
-status_code = response.status_code
-if status_code >= 400:
-    print("Error code:", status_code)
+content = response.text
+if response.status_code >= 400:
+    print(f"Error code: {response.status_code}")
 else:
     print("Regular request")
