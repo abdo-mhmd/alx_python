a2a09b2a7efa2ff811fde40af06a8fca5e79a6a7
abdo-mhmd <abdo202224@gmail.com>
2023-08-19 07:33:07 -0700

    list all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,13 +4,14 @@
 import MySQLdb as s
 import sys
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
     try:
-        db = s.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+        db = s.connect(host="localhost", port=3306,
+                       user=username, passwd=password, db=database)
     except s.Error as e:
         print(e)
 
