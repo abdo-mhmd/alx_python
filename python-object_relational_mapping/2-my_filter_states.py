50266272ae878057650e70942eea07cfbdd52796
abdo-mhmd <abdo202224@gmail.com>
2023-08-19 08:17:22 -0700

    filter by name

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""lists all states from the database 'hbtn_0e_0_usa'"""
+
+import MySQLdb as s
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    name = sys.argv[4]
+
+    try:
+        db = s.connect(host="localhost", port=3306,
+                       user=username, passwd=password, db=database)
+    except s.Error as e:
+        print(e)
+
+    cur = db.cursor()
+    q = "SELECT * FROM states WHERE name = '{}' ORDER BY id".format(name)
+    cur.execute(q)
+
+    for data in cur.fetchall():
+        print(data)
+
+    cur.close()
+    db.close()
