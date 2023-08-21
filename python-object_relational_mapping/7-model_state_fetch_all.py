e1b47d7434ced55c5a61c2e9ac2833f2b86da9f7
abdo-mhmd <abdo202224@gmail.com>
2023-08-21 23:46:00 +0300

    update model state all

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,10 +9,10 @@ import sys
 
 if __name__ == '__main__':
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:4306/{}"
-                           .format(sys.argv[1],sys.argv[2],sys.argv[3]))
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     session = sessionmaker()(bind=engine)
     for state in session.query(State).order_by(State.id):
-        print("{}: {}".format(State.id, State.name))
+        print("{}: {}".format(state.id, state.name))
 
     session.close()
