e26facf887cf5af94e8c076311457937821304b9
abdo-mhmd <abdo202224@gmail.com>
2023-10-01 16:52:26 +0300

    complete all tasks

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -2,27 +2,40 @@
 import json
 import requests
 
-user_url = 'https://jsonplaceholder.typicode.com/users'
-todos_url = 'https://jsonplaceholder.typicode.com/todos'
+def get_all_users_todos():
+    '''
+    Given Task:
+    Create a Python function that utilizes the Trello API to retrieve all the todos associated with a specific user.
+    The function should take the user's Trello API key and return a list of all the todos for useres
+    '''
+    user_url = 'https://jsonplaceholder.typicode.com/users'
+    try:
+        response = requests.get(user_url)
+        users = response.json()
+        all_users_todo = {}
 
-users = requests.get(user_url).json()
-todos = requests.get(todos_url).json()
-users_count = len(users)
+        for user in users:
+            user_id = user['id']
+            todos_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
+            response_todos = requests.get(todos_url)
+            response_todos.raise_for_status()
+            todos = response_todos.json()
+            user_todo_list = []
 
-user = []
-json_obj = {}
+            for todo in todos:
+                user_todo_list.append({
+                    'username': user['username'],
+                    'task': todo['title'],
+                    'completed': todo['completed'],
+                })
 
-for userId in range(users_count):
-    for todo in todos:
-        if todo['userId'] == userId + 1:
-            user.append({
-                'username': users[userId]['username'],
-                'task': todo['title'],
-                'completed': todo['completed']
-            })
+            all_users_todo[user_id] = user_todo_list
 
-    json_obj[userId + 1] = user
-    user = []
+        json_file_name = 'todo_all_employees.json'
+        with open(json_file_name, mode='w') as json_file:
+            json.dump(all_users_todo, json_file)
+    except Exception as e:
+        print(e)
 
-with open('./todo_all_employees.json', 'w') as js:
-    json.dump(json_obj, js)
+if __name__ == '__main__':
+    get_all_users_todos()
